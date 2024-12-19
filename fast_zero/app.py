f599df928f7acc1df5f451dabf29b20701bc4315
Marcos574 <mvdeus574@gmail.com>
2024-12-19 14:09:11 -0300

    Iniciando CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,43 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
+database = []
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {'message': 'Olá mundo!'}
+
+
+@app.get('/hello_html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello_html():
+    return """
+    <html>
+        <head>
+            <title> Olá mundo! </title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+        </body>
+    </html>
+    """
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,70 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange
+@pytest.fixture  # Arrange
+def client():
+    return TestClient(app)
 
+
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
-    
+    assert response.json() == {'message': 'Olá mundo!'}  # Assert
+
+
+def test_hello_world_html(client):
+    response = client.get('hello_html')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert (
+        response.text
+        == """
+    <html>
+        <head>
+            <title> Olá mundo! </title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+        </body>
+    </html>
+    """
+    )
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
