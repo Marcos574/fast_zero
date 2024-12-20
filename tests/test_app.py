708408f468ecbee9db59032f6bee6e779e5767cf
Marcos574 <mvdeus574@gmail.com>
2024-12-20 10:29:04 -0300

    Finalizando o CRUD com fastapi

--- tests/test_app.py
+++ tests/test_app.py
@@ -68,3 +68,48 @@ def test_read_users(client):
             }
         ]
     }
+
+
+def test_update_users(client):
+    user_id = 1
+
+    response = client.put(
+        f'/users/{user_id}',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    response_error = client.put(
+        '/users/0',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': user_id,
+        'username': 'bob',
+        'email': 'bob@example.com',
+    }
+    assert response_error.status_code == HTTPStatus.NOT_FOUND
+    assert response_error.json() == {'detail': 'User not found'}
+
+
+def test_delete_users(client):
+    user_id = 1
+
+    response = client.delete(f'/users/{user_id}')
+
+    response_error = client.delete('/users/0')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+    assert response_error.status_code == HTTPStatus.NOT_FOUND
+    assert response_error.json() == {'detail': 'User not found'}
