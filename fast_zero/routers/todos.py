3b7ea43e37b7cb6ef1c7d3da147d9138d749604b
Marcos574 <mvdeus574@gmail.com>
2024-12-30 14:16:53 -0300

    Avançando na criação da todo list

--- fast_zero/routers/todos.py
+++ fast_zero/routers/todos.py
@@ -3,19 +3,30 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 
 from fast_zero.database import Session, get_session
-from fast_zero.models import User
+from fast_zero.models import Todo, User
 from fast_zero.schemas import TodoPublic, TodoSchema
 from fast_zero.security import get_current_user
 
 router = APIRouter(prefix='/todos', tags=['todos'])
 T_Session = Annotated[Session, Depends(get_session)]
-T_User = Annotated[User, Depends(get_current_user)]
+CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
 @router.post('/', response_model=TodoPublic)
 def create_todo(
     todo: TodoSchema,
     session: T_Session,
-    user: T_User,
+    user: CurrentUser,
 ):
-    return todo
+    db_todo = Todo(
+        title=todo.title,
+        description=todo.description,
+        state=todo.state,
+        user_id=user.id,
+    )
+
+    session.add(db_todo)
+    session.commit()
+    session.refresh(db_todo)
+
+    return db_todo
